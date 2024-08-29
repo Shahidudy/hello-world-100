6c519525a48bb0ba70a34cf28acaeb133cf2ab5a
Muhammad Shahid <shahidudy@gmail.com>
2024-08-29 12:45:15 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/hello_world_100/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hello_world_100
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHome());
+  }
+}
+
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hello world 100'),
+      ),
+    );
+  }
+}
